bce83d2677235bc2953cc061e078f7ee04f0c0ab
mdraw <mdraw.gh@gmail.com>
2018-01-06 01:31:59 +0100

    previews: Many changes to preview plotting
    - In addition to the existing preview predictions that use the
      dedicated constant preview batch (which contains a piece of validation
      data), the last training example of each epoch* is also plotted to
      tensorboard now. This is especially useful for:
      - quick qualitative comparison between model performances on training data
        vs. validation data
      - insight into how training examples typically look like
      - checking if inputs and targets are aligned after all transformations
      - visualizing what effects augmentations can have on training data
      The plotted images (all grouped under the tag "t" in tensorboard) are 2d
      slices of the:
      - input ("tinp")
      - target ("ttarget")
      - raw prediction output of the (currently hardcoded to two) output neurons
        ("tp0", "tp1")
      - The per-voxel index of the most probable class ("tmc")
      I will try to find more meaningful names for those soon.
    - The ground truth of the preview batch and the preview predictions based on
      the preview batch are now both grouped under the tag "p" in tensorboard,
      because they always show the same constant image region.
    - Remove preview_freq for simplicity and because it didn't really work
      as expected. Preview plotting now always happens after each epoch*,
      directly after validation.
    - Don't flush tensorboard writer after every single log operation,
      but flush after all logging calls are finished in the current epoch*.
    - Increase data loader timeouts to 30 seconds
    - Disable multiprocessing for validation data loader.
      Explanation: See the new comment block in validate()
    
    *with "epoch" I am referring to exhaustion of the train_loader iterator,
     which is not really an epoch by common definition, but is completely
     user-controlled via the epoch_size parameter.

--- elektronn3/training/trainer.py
+++ elektronn3/training/trainer.py
@@ -29,7 +29,7 @@ except:
 class StoppableTrainer:
     def __init__(self, model=None, criterion=None, optimizer=None, dataset=None,
                  save_path=None, batchsize=1, num_workers=0,
-                 schedulers=None, preview_freq=20,
+                 schedulers=None,
                  enable_tensorboard=True, tensorboard_root_path='~/tb/',
                  custom_shell=False, cuda_enabled='auto'):
         if cuda_enabled == 'auto':
@@ -57,7 +57,6 @@ class StoppableTrainer:
         else:
             assert type(schedulers) == dict
         self.schedulers = schedulers
-        self.preview_freq = preview_freq
         if not tensorboard_available and enable_tensorboard:
             enable_tensorboard = False
             logger.warning('Tensorboard is not available, so it has to be disabled.')
@@ -68,13 +67,13 @@ class StoppableTrainer:
             tb_dir = os.path.join(self.tensorboard_root_path, self.save_name)
             os.makedirs(tb_dir)
             # TODO: Make always_flush user-configurable here:
-            self.tb = TensorBoardLogger(log_dir=tb_dir, always_flush=True)
+            self.tb = TensorBoardLogger(log_dir=tb_dir, always_flush=False)
         # self.enable_tensorboard = enable_tensorboard  # Using `self.tb not None` instead to check this
         try:
             self.loader = DelayedDataLoader(
                 self.dataset, batch_size=self.batchsize, shuffle=False,
                 num_workers=self.num_workers, pin_memory=self.cuda_enabled,
-                timeout=10  # timeout arg requires https://github.com/pytorch/pytorch/commit/1661370ac5f88ef11fedbeac8d0398e8369fc1f3
+                timeout=30  # timeout arg requires https://github.com/pytorch/pytorch/commit/1661370ac5f88ef11fedbeac8d0398e8369fc1f3
             )
         except:  # TODO: Remove this try/catch once the timeout option is in an official release
             logger.warning(
@@ -113,6 +112,7 @@ class StoppableTrainer:
                 numel = 0
                 target_sum = 0
                 timer = Timer()
+                inp, target, out = None, None, None  # Pre-assign to extend variable scope.
                 for (inp, target) in self.loader:
                     if self.cuda_enabled:
                         inp, target = inp.cuda(), target.cuda()
@@ -122,10 +122,10 @@ class StoppableTrainer:
                     # forward pass
                     out = self.model(inp)
                     # make channels the last axis and flatten
-                    out = out.permute(0, 2, 3, 4, 1).contiguous()
-                    out = out.view(out.numel() // 2, 2)
-                    target = target.view(target.numel())
-                    loss = self.criterion(out, target)  # TODO: Respect class weights
+                    out_ = out.permute(0, 2, 3, 4, 1).contiguous()
+                    out_ = out_.view(out_.numel() // 2, 2)
+                    target_ = target.view(target.numel())
+                    loss = self.criterion(out_, target_)  # TODO: Respect class weights
 
                     # update step
                     self.optimizer.zero_grad()
@@ -133,10 +133,10 @@ class StoppableTrainer:
                     self.optimizer.step()
 
                     # get training performance
-                    numel += target.numel()
-                    target_sum += target.sum()
-                    maxcl = maxclass(out.data)
-                    incorrect += maxcl.ne(target.data).cpu().sum()
+                    numel += target_.numel()
+                    target_sum += target_.sum()
+                    maxcl = maxclass(out_.data)
+                    incorrect += maxcl.ne(target_.data).cpu().sum()
                     tr_loss += loss.data[0]
                     print(f'{self.iterations:6d}, loss: {loss.data[0]:.4f}')
                     self.tracker.update_timeline([self.timer.t_passed, loss.data[0], float(target_sum) / numel])
@@ -160,12 +160,12 @@ class StoppableTrainer:
                                             tr_err, val_err, curr_lr, 0, 0])  # 0's correspond to mom and gradnet (?)
                 t = pretty_string_time(self.timer.t_passed)
                 loss_smooth = self.tracker.loss._ema
-                out = "%05i L_m=%.3f, L=%.2f, tr=%05.2f%%, " % (self.iterations, loss_smooth, tr_loss, tr_err)
-                out += "vl=%05.2f%s, prev=%04.1f, L_diff=%+.1e, " \
+                text = "%05i L_m=%.3f, L=%.2f, tr=%05.2f%%, " % (self.iterations, loss_smooth, tr_loss, tr_err)
+                text += "vl=%05.2f%s, prev=%04.1f, L_diff=%+.1e, " \
                     % (val_err, "%", mean_target * 100, tr_loss_gain)
-                out += "LR=%.5f, %.2f it/s, %s" % (curr_lr, tr_speed, t)
+                text += "LR=%.5f, %.2f it/s, %s" % (curr_lr, tr_speed, t)
                 # TODO: Log voxels/s
-                logger.info(out)
+                logger.info(text)
                 if self.tb:
                     self.tb.log_scalar('stats/tr_loss', tr_loss, self.iterations)
                     self.tb.log_scalar('stats/val_loss', val_loss, self.iterations)
@@ -174,30 +174,53 @@ class StoppableTrainer:
                     self.tb.log_scalar('misc/speed', tr_speed, self.iterations)
                     self.tb.log_scalar('misc/learning_rate', curr_lr, self.iterations)
 
-                    if self.iterations % self.preview_freq == 0:  # TODO: Fix this. Condition is only met with certain epoch_size
-                        _, out = preview_inference(self.model, self.dataset)
-                        mcl = maxclass(out)
-                        # TODO: Less arbitrary slicing
-                        p0 = out[0, 0, 32, ...].data.cpu().numpy()  # class 0
-                        p1 = out[0, 1, 32, ...].data.cpu().numpy()  # class 1
-                        mc = mcl[0, 32, ...].data.cpu().numpy()
+                    # Note that the variables inp, target and out that are used here have their
+                    # values from the last training iteration.
+                    # The prefix "v" for variable names here signifies that
+                    # the content is from the (constant) decdicated preview batch.
+                    # "t" refers to training data from the last training iteration.
 
-                        if self.first_plot:
-                            # ip = inp[0, 0, 32, ...].data.cpu().numpy()
-                            preview_inp, preview_target = self.dataset.preview_batch
-                            inp = preview_inp[0, 0, 32, ...].data.cpu().numpy()
-                            target = preview_target[0, 0, 32, ...].data.cpu().numpy()  # TODO: Target does not match!
-                            try:
-                                self.tb.log_image('gt_input', inp, step=self.iterations)
-                                self.tb.log_image('gt_target', target, step=self.iterations)
-                            except:
-                                IPython.embed()
-                            self.first_plot = False
-                        self.tb.log_image('p/p0', p0, step=self.iterations)
-                        self.tb.log_image('p/p1', p1, step=self.iterations)
-                        self.tb.log_image('p/mc', mc, self.iterations)
-                        # self.tb.log_image('preview', [ip, p0, p1], step=self.iterations)
-                        # TODO: Also plot ground truth target for preview prediction
+                    # TODO: Better variable names! And clean up in general.
+
+                    _, v_out = preview_inference(self.model, self.dataset)
+
+                    # TODO: Less arbitrary slicing
+                    v_plane = v_out.shape[2] // 2
+                    t_plane = out.shape[2] // 2
+
+                    # Preview from constant region of preview batch data
+                    vp0 = v_out[0, 0, v_plane, ...].data.cpu().numpy()  # class 0
+                    vp1 = v_out[0, 1, v_plane, ...].data.cpu().numpy()  # class 1
+                    vmcl = maxclass(v_out)
+                    vmc = vmcl[0, v_plane, ...].data.cpu().numpy()
+
+                    self.tb.log_image('p/vp0', vp0, step=self.iterations)
+                    self.tb.log_image('p/vp1', vp1, step=self.iterations)
+                    self.tb.log_image('p/vmc', vmc, self.iterations)
+
+                    # Preview from last processed training example (random region, possibly augmented)
+                    tinp = inp[0, 0, t_plane, ...].data.cpu().numpy()
+                    ttarget = target[0, 0, t_plane].data.cpu().numpy()
+                    tp0 = out[0, 0, t_plane, ...].data.cpu().numpy()
+                    tp1 = out[0, 1, t_plane, ...].data.cpu().numpy()
+                    tmcl = maxclass(out)
+                    tmc = tmcl[0, t_plane, ...].data.cpu().numpy()
+
+                    self.tb.log_image('t/tinp', tinp, step=self.iterations)
+                    self.tb.log_image('t/ttarget', ttarget, step=self.iterations)
+                    self.tb.log_image('t/tp0', tp0, step=self.iterations)
+                    self.tb.log_image('t/tp1', tp1, step=self.iterations)
+                    self.tb.log_image('t/tmc', tmc, step=self.iterations)
+
+                    if self.first_plot:
+                        preview_inp, preview_target = self.dataset.preview_batch
+                        inp = preview_inp[0, 0, v_plane, ...].data.cpu().numpy()
+                        target = preview_target[0, 0, v_plane, ...].data.cpu().numpy()
+                        self.tb.log_image('p/gt_input', inp, step=self.iterations)
+                        self.tb.log_image('p/gt_target', target, step=self.iterations)
+                        self.first_plot = False
+
+                    self.tb.writer.flush()
 
                 if self.save_path is not None:
                     self.tracker.plot(self.save_path + "/" + self.save_name)
@@ -216,14 +239,23 @@ class StoppableTrainer:
 
     def validate(self):
         if self.valid_loader is None:
+            # num_workers is set to 0 here because background processes for validation sometimes
+            # fail silently and stop responding, bringing down the whole training process.
+            # This issue might be related to https://github.com/pytorch/pytorch/issues/1355,
+            # but the deadlocks described there will only happen if timeout can't be enabled
+            # (PyTorch 0.3.0).
+            # The performance impact of disabling multiprocessing here is low in normal settings,
+            # because the validation loader doesn't perform expensive augmentations, but just reads
+            # data from hdf5s.
+
             try:
                 self.valid_loader = DelayedDataLoader(
-                    self.dataset, self.batchsize, shuffle=False, num_workers=self.num_workers, pin_memory=False,
-                    timeout=10  # timeout arg requires https://github.com/pytorch/pytorch/commit/1661370ac5f88ef11fedbeac8d0398e8369fc1f3
+                    self.dataset, self.batchsize, shuffle=False, num_workers=0, pin_memory=False,
+                    timeout=30  # timeout arg requires https://github.com/pytorch/pytorch/commit/1661370ac5f88ef11fedbeac8d0398e8369fc1f3
                 )
             except:  # TODO: Remove this try/catch once the timeout option is in an official release
                 self.valid_loader = DelayedDataLoader(
-                    self.dataset, self.batchsize, shuffle=False, num_workers=self.num_workers, pin_memory=False
+                    self.dataset, self.batchsize, shuffle=False, num_workers=0, pin_memory=False
                 )
 
         self.dataset.validate()  # Switch dataset to validation sources
