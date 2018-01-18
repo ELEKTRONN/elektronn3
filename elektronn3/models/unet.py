c5c93df4cde5ad71ea4c617a19ce44c2f5b63829
mdraw <mdraw.gh@gmail.com>
2018-01-18 19:11:30 +0100

    unet: Change defaults for our standard use case

--- elektronn3/models/unet.py
+++ elektronn3/models/unet.py
@@ -158,7 +158,7 @@ class UNet(nn.Module):
         the tranpose convolution (specified by upmode='transpose')
     """
 
-    def __init__(self, num_classes, in_channels=3, depth=5,
+    def __init__(self, num_classes=2, in_channels=1, depth=5,
                  start_filts=64, up_mode='transpose',
                  merge_mode='concat'):
         """
