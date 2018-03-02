4f1b639b23f36278a17db1df817cd1e764c0c3c9
mdraw <mdraw.gh@gmail.com>
2018-03-02 05:06:04 +0100

    trainer: Clean up
    - Remove .data() calls on tensors because they are no longer needed
    - Tensors with requires_grad=True that are converted to numpy
      need to be .detach()-ed from the autograd graph (even in torch.no_grad()
      contexts... -> PyTorch bug?)
    - Clean up valid_loader initialization (expect timeout option to be
      available)

--- elektronn3/training/trainer.py
+++ elektronn3/training/trainer.py
@@ -147,11 +147,11 @@ class StoppableTrainer:
                     # get training performance
                     numel += int(target_.numel())
                     target_sum += int(target_.sum())
-                    maxcl = maxclass(out_.data)
+                    maxcl = maxclass(out_)
                     # .ne() creates a ByteTensor, which leads to integer
                     # overflows when it is sum-reduced. Therefore it's
                     # necessary to cast to a LongTensor before reducing.
-                    incorrect += int(maxcl.ne(target_.data).long().sum())
+                    incorrect += int(maxcl.ne(target_).long().sum())
                     floss = float(loss)
                     tr_loss += floss
                     print(f'{self.iterations:6d}, loss: {floss:.4f}')
@@ -207,22 +207,31 @@ class StoppableTrainer:
                     t_plane = out.shape[2] // 2
 
                     # Preview from constant region of preview batch data
-                    vp0 = v_out[0, 0, v_plane, ...].data.cpu().numpy()  # class 0
-                    vp1 = v_out[0, 1, v_plane, ...].data.cpu().numpy()  # class 1
+                    vp0 = v_out[0, 0, v_plane, ...].cpu().numpy()  # class 0
+                    vp1 = v_out[0, 1, v_plane, ...].cpu().numpy()  # class 1
                     vmcl = maxclass(v_out)
-                    vmc = vmcl[0, v_plane, ...].data.cpu().numpy()
+                    vmc = vmcl[0, v_plane, ...].cpu().numpy()
 
                     self.tb.log_image('p/vp0', vp0, step=self.iterations)
                     self.tb.log_image('p/vp1', vp1, step=self.iterations)
                     self.tb.log_image('p/vmc', vmc, self.iterations)
 
                     # Preview from last processed training example (random region, possibly augmented)
-                    tinp = inp[0, 0, t_plane, ...].data.cpu().numpy()
-                    ttarget = target[0, 0, t_plane].data.cpu().numpy()
-                    tp0 = out[0, 0, t_plane, ...].data.cpu().numpy()
-                    tp1 = out[0, 1, t_plane, ...].data.cpu().numpy()
+
+                    # inp and out come directly from the last training step,
+                    # so they still have requires_grad=True.
+                    # They need to be detached from the graph for conversion
+                    # to numpy (this is safe because gradients have already
+                    # been computed and the actual training step is over).
+                    inp = inp.detach()
+                    out = out.detach()
+
+                    tinp = inp[0, 0, t_plane, ...].cpu().numpy()
+                    ttarget = target[0, 0, t_plane].cpu().numpy()
+                    tp0 = out[0, 0, t_plane, ...].cpu().numpy()
+                    tp1 = out[0, 1, t_plane, ...].cpu().numpy()
                     tmcl = maxclass(out)
-                    tmc = tmcl[0, t_plane, ...].data.cpu().numpy()
+                    tmc = tmcl[0, t_plane, ...].cpu().numpy()
 
                     self.tb.log_image('t/tinp', tinp, step=self.iterations)
                     self.tb.log_image('t/ttarget', ttarget, step=self.iterations)
@@ -232,8 +241,8 @@ class StoppableTrainer:
 
                     if self.first_plot:
                         preview_inp, preview_target = self.dataset.preview_batch
-                        inp = preview_inp[0, 0, v_plane, ...].data.cpu().numpy()
-                        target = preview_target[0, 0, v_plane, ...].data.cpu().numpy()
+                        inp = preview_inp[0, 0, v_plane, ...].cpu().numpy()
+                        target = preview_target[0, 0, v_plane, ...].cpu().numpy()
                         self.tb.log_image('p/gt_input', inp, step=self.iterations)
                         self.tb.log_image('p/gt_target', target, step=self.iterations)
                         self.first_plot = False
@@ -282,16 +291,10 @@ class StoppableTrainer:
             # The performance impact of disabling multiprocessing here is low in normal settings,
             # because the validation loader doesn't perform expensive augmentations, but just reads
             # data from hdf5s.
-
-            try:
-                self.valid_loader = DelayedDataLoader(
-                    self.dataset, self.batchsize, shuffle=False, num_workers=0, pin_memory=False,
-                    timeout=30  # timeout arg requires https://github.com/pytorch/pytorch/commit/1661370ac5f88ef11fedbeac8d0398e8369fc1f3
-                )
-            except:  # TODO: Remove this try/catch once the timeout option is in an official release
-                self.valid_loader = DelayedDataLoader(
-                    self.dataset, self.batchsize, shuffle=False, num_workers=0, pin_memory=False
-                )
+            self.valid_loader = DelayedDataLoader(
+                self.dataset, self.batchsize, num_workers=0, pin_memory=False,
+                timeout=30
+            )
 
         self.dataset.validate()  # Switch dataset to validation sources
         self.model.eval()  # Set dropout and batchnorm to eval mode
@@ -309,21 +312,21 @@ class StoppableTrainer:
                 target = target.view(target.numel())
                 numel += int(target.numel())
                 val_loss += float(self.criterion(out, target))  # TODO: Respect class weights
-                maxcl = maxclass(out.data)  # get the index of the max log-probability
-                incorrect += int(maxcl.ne(target.data).long().sum())
+                maxcl = maxclass(out)  # get the index of the max log-probability
+                incorrect += int(maxcl.ne(target).long().sum())
         val_loss /= len(self.valid_loader)  # loss function already averages over batch size
         val_err = 100. * incorrect / numel
         if self.save_path is not None:
             write_overlayimg(
                 "%s/" % (self.save_path),
-                inp.data.view(inp.size())[0, 0].cpu().numpy(),
+                inp.view(inp.size())[0, 0].cpu().numpy(),
                 maxcl.view(inp.size())[0, 0].cpu().numpy(),
                 fname="%d_overlay" % self.iterations,
                 nb_of_slices=2
             )
             imsave(
                 "%s/%d_target.png" % (self.save_path, self.iterations),
-                target.data.view(inp.size())[0, 0, 8].cpu().numpy()
+                target.view(inp.size())[0, 0, 8].cpu().numpy()
             )
 
         # Reset dataset and model to training mode
@@ -359,7 +362,7 @@ def save_to_h5(fname: str, model_output: Variable):
         hdf5_names=["pred", "raw"]
     )
     save_to_h5py(
-        [np.exp(model_output.data.view([1, 2, shape[0], shape[1], shape[2]])[0, 1].cpu().numpy(), dtype=np.float32)],
+        [np.exp(model_output.view([1, 2, shape[0], shape[1], shape[2]])[0, 1].cpu().numpy(), dtype=np.float32)],
         fname+"prob.h5",
         hdf5_names=["prob"]
     )
