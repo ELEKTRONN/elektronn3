c87f66617b7fd4fbfcb5f94c42d56df1d9bba75d
Philipp Schubert <p.schubert@stud.uni-heidelberg.de>
2019-02-18 13:11:59 +0100

    fixed bug in refactored version of RandomFlip augmentation which did not flip the spatial axes of input and target but the first 2 (B, C in raw and B, X in target ..)

--- elektronn3/data/transforms/transforms.py
+++ elektronn3/data/transforms/transforms.py
@@ -474,17 +474,30 @@ class RandomFlip:
             inp: np.ndarray,
             target: Optional[np.ndarray] = None
     ) -> Tuple[np.ndarray, np.ndarray]:
-        flip_dims = self.noise_generator(self.ndim_spatial)
-        for dim in range(self.ndim_spatial):
-            if flip_dims[dim]:
-                inp = np.flip(inp, dim)
-                # PyTorch DataLoader doesn't support negative strides, so we
-                #  have to remove them by forcing contiguous memory layout.
-                inp = np.ascontiguousarray(inp)
-                if target is not None:
-                    target = np.flip(target, dim)
-                    target = np.ascontiguousarray(target)
-        return inp, target
+        # flip_dims = self.noise_generator(self.ndim_spatial)
+        # for dim in range(self.ndim_spatial):
+        #     if flip_dims[dim]:
+        #         inp = np.flip(inp, -dim)
+        #         # PyTorch DataLoader doesn't support negative strides, so we
+        #         #  have to remove them by forcing contiguous memory layout.
+        #         inp = np.ascontiguousarray(inp)
+        #         if target is not None:
+        #             target = np.flip(target, -dim)
+        #             target = np.ascontiguousarray(target)
+        # return inp, target
+        flip_dims = np.random.randint(0, 2, self.ndim_spatial)
+        # flip all images at once
+        slices_inp = tuple([slice(None, None, 1) for _ in range(len(inp.shape) - self.ndim_spatial)] + \
+                 [slice(None, None, (-1)**flip_d) for flip_d in flip_dims])
+        inp_flipped = inp[slices_inp].copy()
+        if target is not None:
+            slices_target = tuple([slice(None, None, 1) for _ in range(len(target.shape) - self.ndim_spatial)] + \
+                     [slice(None, None, (-1)**flip_d) for flip_d in flip_dims])
+            target_flipped = target[slices_target].copy()
+        else:
+            target_flipped = None
+        return inp_flipped, target_flipped
+
 
 
 # TODO: Functional API (transforms.functional).
