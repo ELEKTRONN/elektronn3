25e830d4ae8a9eade19984bfc7679752d2599093
mdraw <martin.drawitsch@gmail.com>
2019-04-15 18:22:32 +0200

    loss: Rewrite dice loss
    - Make reduction work more like the original GDL loss, reducing
      numerator and denominator separately by sum-reduction.
      The previous formulation was working rather well, but it wasn't really
      the same as the Dice Loss idea in https://arxiv.org/abs/1707.03237 and
      https://arxiv.org/abs/1606.04797.
    - Support weights of arbitrary shapes that only have to be broadcastable
      to probs shapes. This means weights can be scalar or per-channel (as
      before), but they can now also be dense weight maps with spatial
      dimensions
    - Add support for additive smoothing
    - Add type hints
    - Clearer implementation
    - Don't depend on _channelwise_sum function - dice_loss() now only
      depends on torch itself
    - Add support for disabling reduction - making the return values dense
      tensors (usable for visualization and custom loss implementations)

--- elektronn3/modules/loss.py
+++ elektronn3/modules/loss.py
@@ -11,16 +11,17 @@ import torch
 from elektronn3.modules.lovasz_losses import lovasz_softmax
 
 
-def _channelwise_sum(x: torch.Tensor):
-    """Sum-reduce all dimensions of a tensor except dimension 1 (C)"""
-    reduce_dims = tuple([0] + list(range(x.dim()))[2:])  # = (0, 2, 3, ...)
-    return x.sum(dim=reduce_dims)
-
-
-# TODO: Dense weight support
-def dice_loss(probs, target, weight=1., eps=0.0001):
+def dice_loss(
+        probs: torch.Tensor,
+        target: torch.Tensor,
+        weight: torch.Tensor = torch.tensor(1.),
+        eps: float = 0.0001,
+        smooth: float = 0.0,
+        reduce: bool = True
+) -> torch.Tensor:
     # Probs need to be softmax probabilities, not raw network outputs
     tsh, psh = target.shape, probs.shape
+    n, c = psh[:2]
 
     if tsh == psh:  # Already one-hot
         onehot_target = target.to(probs.dtype)
@@ -31,22 +32,29 @@ def dice_loss(probs, target, weight=1., eps=0.0001):
         raise ValueError(
             f'Target shape {target.shape} is not compatible with output shape {probs.shape}.'
         )
-    # if weight is None:
-    #     weight = torch.ones(probs.shape[0], dtype=probs.dtype)  # (C,)
     # if ignore_index is not None:
     #     weight[:, ignore_index] = 0.
 
-    intersection = probs * onehot_target  # (N, C, ...)
-    numerator = 2 * _channelwise_sum(intersection)  # (C,)
-    denominator = probs + onehot_target  # (N, C, ...)
-    denominator = _channelwise_sum(denominator) + eps  # (C,)
-    loss_per_channel = 1 - (numerator / denominator)  # (C,)
-    weighted_loss_per_channel = weight * loss_per_channel  # (C,)
-    return weighted_loss_per_channel.mean()  # ()
+    # Reshape tensors/collapse spatial dimensions into one
+    probs_flat = probs.view(n, c, -1)  # (N, C, S), where S = D*H*W (or H*W etc.)
+    onehot_target_flat = onehot_target.view(n, c, -1)  # (N, C, S)
+    weight_flat = weight.view(1, c, -1)  # (1, C, 1) (broadcastable to (N, C, S)
+
+    numerator = 2 * weight_flat * (probs_flat * onehot_target_flat)  # (N, C, S)
+    denominator = weight_flat * (probs_flat + onehot_target_flat)  # (N, C, S)
+    if reduce:  # Sum-reduce both numerator and denominator separately
+        numerator = torch.sum(numerator)
+        denominator = torch.sum(denominator)
+    else:
+        numerator = numerator.view(psh)
+        denominator = denominator.view(psh)
+    loss = 1 - ((numerator + smooth) / (denominator + smooth + eps))
+    return loss
 
 
 class DiceLoss(torch.nn.Module):
-    """Generalized Dice Loss, as described in https://arxiv.org/abs/1707.03237.
+    """Generalized Dice Loss, similar to the GDL loss described in
+    https://arxiv.org/abs/1707.03237.
 
     Works for n-dimensional data. Assuming that the ``output`` tensor to be
     compared to the ``target`` has the shape (N, C, D, H, W), the ``target``
@@ -62,21 +70,52 @@ class DiceLoss(torch.nn.Module):
             your model does not already apply softmax as the last layer.
             If ``False``, ``output`` is assumed to already contain softmax
             probabilities.
-        weight: Weight tensor for class-wise loss rescaling.
-            Has to be of shape (C,). If ``None``, classes are weighted equally.
+        weight: Weight tensor for loss rescaling.
+            Has to be of a shape that is broadcastable to the target shape
+            or scalar or a tensor of shape (C,).
+            By default, no weighting is performed.
+        eps: Small float value to avoid divisions by zero. Can be set to 0 if
+            ``smooth`` > 0.
+        smooth: Additive smoothing value. See
+            https://github.com/pytorch/pytorch/issues/1249#issuecomment-337999895.
+        reduce: If ``True`` (default), sum-reduce both the numerator and
+            the denominator, resulting in a scalar return value (which you
+            can call ``.backward()`` on). If ``False``, this step is skipped
+            and the returned tensor will be of the same shape as the
+            ``output``, so it can't be directly be ``.backward()``-ed.
+            (Note: "mean"-reduction is not supported because it is
+            mathematically equivalent to sum-reduction in this loss
+            formulation.)
     """
-    def __init__(self, apply_softmax=True, weight=torch.tensor(1.)):
+    def __init__(
+            self,
+            apply_softmax: bool = True,
+            weight: torch.Tensor = torch.tensor(1.),
+            eps: float = 0.0001,
+            smooth: float = 0.0,
+            reduce: bool = True
+    ):
         super().__init__()
         if apply_softmax:
-            self.softmax = torch.nn.Softmax(dim=1)
+            self.maybe_softmax = torch.nn.Softmax(dim=1)
         else:
-            self.softmax = lambda x: x  # Identity (no softmax)
+            self.maybe_softmax = lambda x: x  # Identity (no softmax)
         self.dice = dice_loss
+        self.eps = eps
+        self.smooth = smooth
+        self.reduce = reduce
         self.register_buffer('weight', weight)
 
     def forward(self, output, target):
-        probs = self.softmax(output)
-        return self.dice(probs, target, weight=self.weight)
+        probs = self.maybe_softmax(output)
+        return self.dice(
+            probs,
+            target,
+            weight=self.weight,
+            eps=self.eps,
+            smooth=self.smooth,
+            reduce=self.reduce
+        )
 
 
 class LovaszLoss(torch.nn.Module):
@@ -94,51 +133,3 @@ class LovaszLoss(torch.nn.Module):
     def forward(self, output, target):
         probs = self.softmax(output)
         return self.lovasz(probs, target)
-
-
-##### ALTERNATIVE VERSIONS OF DICE LOSS #####
-
-# Version with features that are untested and currently not needed
-# Based on https://discuss.pytorch.org/t/one-hot-encoding-with-autograd-dice-loss/9781/5
-def __dice_loss_with_cool_extra_features(output, target, weights=None, ignore_index=None):
-    eps = 0.0001
-
-    encoded_target = torch.zeros_like(output)
-    if ignore_index is not None:
-        mask = target == ignore_index
-        target = target.clone()
-        target[mask] = 0
-        encoded_target.scatter_(1, target.unsqueeze(1), 1)
-        mask = mask.unsqueeze(1).expand_as(encoded_target)
-        encoded_target[mask] = 0
-    else:
-        encoded_target.scatter_(1, target.unsqueeze(1), 1)
-
-    if weights is None:
-        weights = 1
-
-    intersection = output * encoded_target
-    numerator = 2 * _channelwise_sum(intersection)
-    denominator = output + encoded_target
-
-    if ignore_index is not None:
-        denominator[mask] = 0
-    denominator = _channelwise_sum(denominator) + eps
-    loss_per_channel = weights * (1 - (numerator / denominator))
-
-    return loss_per_channel.sum() / output.shape[1]
-
-
-# Very simple version. Only for binary classification. Just included for testing.
-# Note that the smooth value is set to 0 and eps is introduced instead, to make it comparable.
-# Based on https://github.com/pytorch/pytorch/issues/1249#issuecomment-305088398
-def __dice_loss_binary(output, target, smooth=0, eps=0.0001):
-    onehot_target = torch.zeros_like(output)
-    onehot_target.scatter_(1, target.unsqueeze(1), 1)
-
-    iflat = output.view(-1)
-    tflat = onehot_target.view(-1)
-    intersection = (iflat * tflat).sum()
-
-    return 1 - ((2 * intersection + smooth) /
-                (iflat.sum() + tflat.sum() + smooth + eps))
