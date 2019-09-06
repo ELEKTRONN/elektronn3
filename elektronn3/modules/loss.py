4dfd60e65061946e18dd3f42aefa20785e1217fd
mdraw <martin.drawitsch@gmail.com>
2019-09-06 14:35:52 +0200

    loss: Add SoftmaxBCELoss

--- elektronn3/modules/loss.py
+++ elektronn3/modules/loss.py
@@ -45,6 +45,26 @@ class CombinedLoss(torch.nn.Module):
         return loss
 
 
+class SoftmaxBCELoss(torch.nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.bce = torch.nn.BCELoss(*args, **kwargs)
+
+    def forward(self, output, target):
+        probs = torch.nn.functional.softmax(output, dim=1)
+        return self.bce(output, target)
+
+# class MultiLabelCrossEntropy(nn.Module):
+#     def __init__(self, weight=torch.tensor(1.)):
+#         self.register_buffer('weight', weight)
+
+#     def forward(self, output, target):
+#         assert output.shape == target.shape
+#         logprobs = torch.nn.functional.log_softmax(output, dim=1)
+#         wsum = self.weight[None] * torch.sum(-target * logprobs)
+#         return torch.mean(wsum, dim=1)
+
+
 def _channelwise_sum(x: torch.Tensor):
     """Sum-reduce all dimensions of a tensor except dimension 1 (C)"""
     reduce_dims = tuple([0] + list(range(x.dim()))[2:])  # = (0, 2, 3, ...)
