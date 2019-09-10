3471dddb56b44273d70f1368c0162be83c84cd2b
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2019-09-10 02:47:43 +0200

    configurable argmax_with_threshold

--- elektronn3/inference/inference.py
+++ elektronn3/inference/inference.py
@@ -37,6 +37,7 @@ def tiled_apply(
         overlap_shape: Sequence[int],
         offset: Optional[Sequence[int]],
         out_shape: Sequence[int],
+        argmax_with_threshold = None,
         verbose: bool = False
 ) -> torch.Tensor:
     """Splits a tensor into overlapping tiles and applies a function on them independently.
@@ -96,6 +97,7 @@ def tiled_apply(
             later.
         verbose: If ``True``, a progress bar will be shown while iterating over
             the tiles.
+        argmax_with_threshold
 Tensor
     Returns:
         Output tensor, as a torch tensor of the same shape as the input tensor.
@@ -117,7 +119,8 @@ Tensor
     else:
         offset = np.array(offset)
     inp_shape = np.array(inp.shape)
-    out = torch.empty(out_shape, dtype=torch.uint8, device='cpu')
+    out_dtype = torch.uint8 if argmax_with_threshold is not None else dtype
+    out = torch.empty(out_shape, dtype=out_dtype, device='cpu')
     out_shape = np.array(out.shape)
     tile_shape = np.array(tile_shape)
     overlap_shape = np.array(overlap_shape)
@@ -180,7 +183,10 @@ Tensor
         # Slice the relevant tile_shape-sized region out of the model output
         #  so it can be written to the final output
         out_tile = out_tile[final_crop_slice]
-        out[out_slice] = (out_tile > 0.5).argmax(dim=1).to(torch.uint8)
+        if argmax_with_threshold is not None:
+            out[out_slice] = (out_tile > argmax_with_threshold).argmax(dim=1).to(torch.uint8)
+        else:
+            out[out_slice] = out_tile
 
     return out
 
@@ -290,6 +296,7 @@ class Predictor:
             float16: bool = False,
             apply_softmax: bool = True,
             strict_shapes: bool = False,
+            argmax_with_threshold = None,
             verbose: bool = False,
     ):
         if device is None:
@@ -318,6 +325,7 @@ class Predictor:
             )
         self.dtype = torch.float16 if float16 else torch.float32
         self.strict_shapes = strict_shapes
+        self.argmax_with_threshold = argmax_with_threshold
         self.verbose = verbose
         if isinstance(model, str):
             if os.path.isfile(model):
@@ -376,6 +384,7 @@ class Predictor:
                 overlap_shape=self.overlap_shape,
                 offset=self.offset,
                 out_shape=out_shape,
+                argmax_with_threshold=self.argmax_with_threshold,
                 verbose=self.verbose
             )
         # Otherwise: No tiling, apply model to the whole input in one step
