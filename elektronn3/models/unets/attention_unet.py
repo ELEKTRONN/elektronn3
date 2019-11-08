a81722dce0faa4c8971d2872d3b0ee84788ab991
YangLiu14 <yliu@wb01.wb.mpcdf.mpg.de>
2019-11-08 11:16:51 +0100

    add nested-unet, attention-unet, rcnn-attention-unet

---
+++ elektronn3/models/unets/attention_unet.py
@@ -0,0 +1,313 @@
+# ELEKTRONN3 - Neural Network Toolkit
+#
+# Copyright (c) 2017 - now
+# Max Planck Institute of Neurobiology, Munich, Germany
+# Author: Martin Drawitsch
+
+"""
+TODO: docstring
+original implementation on:
+https://github.com/bigmb/Unet-Segmentation-Pytorch-Nest-of-Unets
+"""
+
+from __future__ import print_function, division
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data
+import torch
+
+
+class conv_block(nn.Module):
+    """
+    Convolution Block
+    """
+
+    def __init__(self, in_ch, out_ch):
+        super(conv_block, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True))
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class up_conv(nn.Module):
+    """
+    Up Convolution Block
+    """
+    def __init__(self, in_ch, out_ch):
+        super(up_conv, self).__init__()
+        self.up = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.up(x)
+        return x
+
+# ============================
+# Attention Unet
+# ============================
+
+class Attention_block(nn.Module):
+    """
+    Attention Block
+    """
+
+    def __init__(self, F_g, F_l, F_int):
+        super(Attention_block, self).__init__()
+
+        self.W_g = nn.Sequential(
+            nn.Conv2d(F_l, F_int, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_g, F_int, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+
+        self.psi = nn.Sequential(
+            nn.Conv2d(F_int, 1, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid()
+        )
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, g, x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1 + x1)
+        psi = self.psi(psi)
+        out = x * psi
+        return out
+
+
+class AttU_Net(nn.Module):
+    """
+    Attention Unet implementation
+    Paper: https://arxiv.org/abs/1804.03999
+    """
+    def __init__(self, in_channels=3, out_channels=1):
+        super(AttU_Net, self).__init__()
+
+        n1 = 64
+        filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
+
+        self.Maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.Maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.Maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.Maxpool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.Conv1 = conv_block(in_channels, filters[0])
+        self.Conv2 = conv_block(filters[0], filters[1])
+        self.Conv3 = conv_block(filters[1], filters[2])
+        self.Conv4 = conv_block(filters[2], filters[3])
+        self.Conv5 = conv_block(filters[3], filters[4])
+
+        self.Up5 = up_conv(filters[4], filters[3])
+        self.Att5 = Attention_block(F_g=filters[3], F_l=filters[3], F_int=filters[2])
+        self.Up_conv5 = conv_block(filters[4], filters[3])
+
+        self.Up4 = up_conv(filters[3], filters[2])
+        self.Att4 = Attention_block(F_g=filters[2], F_l=filters[2], F_int=filters[1])
+        self.Up_conv4 = conv_block(filters[3], filters[2])
+
+        self.Up3 = up_conv(filters[2], filters[1])
+        self.Att3 = Attention_block(F_g=filters[1], F_l=filters[1], F_int=filters[0])
+        self.Up_conv3 = conv_block(filters[2], filters[1])
+
+        self.Up2 = up_conv(filters[1], filters[0])
+        self.Att2 = Attention_block(F_g=filters[0], F_l=filters[0], F_int=32)
+        self.Up_conv2 = conv_block(filters[1], filters[0])
+
+        self.Conv = nn.Conv2d(filters[0], out_channels, kernel_size=1, stride=1, padding=0)
+
+        #self.active = torch.nn.Sigmoid()
+
+
+    def forward(self, x):
+
+        e1 = self.Conv1(x)
+
+        e2 = self.Maxpool1(e1)
+        e2 = self.Conv2(e2)
+
+        e3 = self.Maxpool2(e2)
+        e3 = self.Conv3(e3)
+
+        e4 = self.Maxpool3(e3)
+        e4 = self.Conv4(e4)
+
+        e5 = self.Maxpool4(e4)
+        e5 = self.Conv5(e5)
+
+        #print(x5.shape)
+        d5 = self.Up5(e5)
+        #print(d5.shape)
+        x4 = self.Att5(g=d5, x=e4)
+        d5 = torch.cat((x4, d5), dim=1)
+        d5 = self.Up_conv5(d5)
+
+        d4 = self.Up4(d5)
+        x3 = self.Att4(g=d4, x=e3)
+        d4 = torch.cat((x3, d4), dim=1)
+        d4 = self.Up_conv4(d4)
+
+        d3 = self.Up3(d4)
+        x2 = self.Att3(g=d3, x=e2)
+        d3 = torch.cat((x2, d3), dim=1)
+        d3 = self.Up_conv3(d3)
+
+        d2 = self.Up2(d3)
+        x1 = self.Att2(g=d2, x=e1)
+        d2 = torch.cat((x1, d2), dim=1)
+        d2 = self.Up_conv2(d2)
+
+        out = self.Conv(d2)
+
+      #  out = self.active(out)
+
+        return out
+
+# ============================
+# RCNN - Attention Unet
+# ============================
+class Recurrent_block(nn.Module):
+    """
+    Recurrent Block for R2Unet_CNN
+    """
+    def __init__(self, out_ch, t=2):
+        super(Recurrent_block, self).__init__()
+
+        self.t = t
+        self.out_ch = out_ch
+        self.conv = nn.Sequential(
+            nn.Conv2d(out_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        for i in range(self.t):
+            if i == 0:
+                x = self.conv(x)
+            out = self.conv(x + x)
+        return out
+
+
+class RRCNN_block(nn.Module):
+    """
+    Recurrent Residual Convolutional Neural Network Block
+    """
+    def __init__(self, in_ch, out_ch, t=2):
+        super(RRCNN_block, self).__init__()
+
+        self.RCNN = nn.Sequential(
+            Recurrent_block(out_ch, t=t),
+            Recurrent_block(out_ch, t=t)
+        )
+        self.Conv = nn.Conv2d(in_ch, out_ch, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        x1 = self.Conv(x)
+        x2 = self.RCNN(x1)
+        out = x1 + x2
+        return out
+
+
+class R2AttU_Net(nn.Module):
+    """
+    Residual Recuurent Block with attention Unet
+    Implementation : https://github.com/LeeJunHyun/Image_Segmentation
+    """
+    def __init__(self, in_channels=3, out_channels=1, t=2):
+        super(R2AttU_Net, self).__init__()
+
+        n1 = 64
+        filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
+
+        self.Maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.Maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.Maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.Maxpool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.RRCNN1 = RRCNN_block(in_channels, filters[0], t=t)
+        self.RRCNN2 = RRCNN_block(filters[0], filters[1], t=t)
+        self.RRCNN3 = RRCNN_block(filters[1], filters[2], t=t)
+        self.RRCNN4 = RRCNN_block(filters[2], filters[3], t=t)
+        self.RRCNN5 = RRCNN_block(filters[3], filters[4], t=t)
+
+        self.Up5 = up_conv(filters[4], filters[3])
+        self.Att5 = Attention_block(F_g=filters[3], F_l=filters[3], F_int=filters[2])
+        self.Up_RRCNN5 = RRCNN_block(filters[4], filters[3], t=t)
+
+        self.Up4 = up_conv(filters[3], filters[2])
+        self.Att4 = Attention_block(F_g=filters[2], F_l=filters[2], F_int=filters[1])
+        self.Up_RRCNN4 = RRCNN_block(filters[3], filters[2], t=t)
+
+        self.Up3 = up_conv(filters[2], filters[1])
+        self.Att3 = Attention_block(F_g=filters[1], F_l=filters[1], F_int=filters[0])
+        self.Up_RRCNN3 = RRCNN_block(filters[2], filters[1], t=t)
+
+        self.Up2 = up_conv(filters[1], filters[0])
+        self.Att2 = Attention_block(F_g=filters[0], F_l=filters[0], F_int=32)
+        self.Up_RRCNN2 = RRCNN_block(filters[1], filters[0], t=t)
+
+        self.Conv = nn.Conv2d(filters[0], out_channels, kernel_size=1, stride=1, padding=0)
+
+       # self.active = torch.nn.Sigmoid()
+
+
+    def forward(self, x):
+
+        e1 = self.RRCNN1(x)
+
+        e2 = self.Maxpool1(e1)
+        e2 = self.RRCNN2(e2)
+
+        e3 = self.Maxpool2(e2)
+        e3 = self.RRCNN3(e3)
+
+        e4 = self.Maxpool3(e3)
+        e4 = self.RRCNN4(e4)
+
+        e5 = self.Maxpool4(e4)
+        e5 = self.RRCNN5(e5)
+
+        d5 = self.Up5(e5)
+        e4 = self.Att5(g=d5, x=e4)
+        d5 = torch.cat((e4, d5), dim=1)
+        d5 = self.Up_RRCNN5(d5)
+
+        d4 = self.Up4(d5)
+        e3 = self.Att4(g=d4, x=e3)
+        d4 = torch.cat((e3, d4), dim=1)
+        d4 = self.Up_RRCNN4(d4)
+
+        d3 = self.Up3(d4)
+        e2 = self.Att3(g=d3, x=e2)
+        d3 = torch.cat((e2, d3), dim=1)
+        d3 = self.Up_RRCNN3(d3)
+
+        d2 = self.Up2(d3)
+        e1 = self.Att2(g=d2, x=e1)
+        d2 = torch.cat((e1, d2), dim=1)
+        d2 = self.Up_RRCNN2(d2)
+
+        out = self.Conv(d2)
+
+      #  out = self.active(out)
+
+        return out
