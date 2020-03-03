390bb58d32910b2a614c7ca2663dd0004db52383
mdraw <martin.drawitsch@gmail.com>
2020-03-03 03:58:04 +0100

    transforms: Return ones instead of SEDT...
    if the target is empty in DistanceTransformTarget

--- elektronn3/data/transforms/transforms.py
+++ elektronn3/data/transforms/transforms.py
@@ -243,9 +243,13 @@ class DistanceTransformTarget:
         self.signed = signed
         self.vector = vector
 
-    def edt(self, target):
+    def edt(self, target: np.ndarray) -> np.ndarray:
+        sh = target.shape
+        if target.min() == 1:  # If everything is 1, the EDT should be inf for every pixel
+            nc = target.ndim if self.vector else 1
+            return np.full((nc, *sh), np.inf, dtype=np.float32)
+
         if self.vector:
-            sh = target.shape
             if target.ndim == 2:
                 coords = np.mgrid[:sh[0], :sh[1]]
             elif target.ndim == 3:
@@ -257,8 +261,10 @@ class DistanceTransformTarget:
             ).astype(np.float32)
             dist = inds - coords
             # assert np.isclose(np.sqrt(dist[0] ** 2 + dist[1] ** 2), distance_transform_edt(target))
-        else:
-            dist = distance_transform_edt(target).astype(np.float32)[None]
+            return dist
+
+        # Else: Regular scalar edt
+        dist = distance_transform_edt(target).astype(np.float32)[None]
         return dist
 
     def __call__(
