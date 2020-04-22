eaf1788c4e08b8019e161f725a68525e72c3cfd9
PhylomatX <klimesch.jonathan@gmail.com>
2020-04-22 12:26:01 +0200

    Added more normalization options.

--- elektronn3/models/convpoint.py
+++ elektronn3/models/convpoint.py
@@ -297,7 +297,8 @@ class SegSmall(nn.Module):
 
 
 class SegBig(nn.Module):
-    def __init__(self, input_channels, output_channels, trs=False, dimension=3, dropout=0):
+    def __init__(self, input_channels, output_channels, trs=False, dimension=3, dropout=0, use_bias=False,
+                 norm_type='bn'):
         super(SegBig, self).__init__()
 
         n_centers = 16
@@ -306,42 +307,59 @@ class SegBig(nn.Module):
         pl = 64
 
         # 64 convolutional kernels
-        self.cv0 = PtConv(input_channels, pl, n_centers, dimension, use_bias=False)
-        self.cv1 = PtConv(pl, pl, n_centers, dimension, use_bias=False)
-        self.cv2 = PtConv(pl, pl, n_centers, dimension, use_bias=False)
-        self.cv3 = PtConv(pl, pl, n_centers, dimension, use_bias=False)
+        self.cv0 = PtConv(input_channels, pl, n_centers, dimension, use_bias=use_bias)
+        self.cv1 = PtConv(pl, pl, n_centers, dimension, use_bias=use_bias)
+        self.cv2 = PtConv(pl, pl, n_centers, dimension, use_bias=use_bias)
+        self.cv3 = PtConv(pl, pl, n_centers, dimension, use_bias=use_bias)
 
         # 128 convolutional kernels
-        self.cv4 = PtConv(pl, 2 * pl, n_centers, dimension, use_bias=False)
-        self.cv5 = PtConv(2 * pl, 2 * pl, n_centers, dimension, use_bias=False)
-        self.cv6 = PtConv(2 * pl, 2 * pl, n_centers, dimension, use_bias=False)
-        self.cv5d = PtConv(2 * pl, 2 * pl, n_centers, dimension, use_bias=False)
+        self.cv4 = PtConv(pl, 2 * pl, n_centers, dimension, use_bias=use_bias)
+        self.cv5 = PtConv(2 * pl, 2 * pl, n_centers, dimension, use_bias=use_bias)
+        self.cv6 = PtConv(2 * pl, 2 * pl, n_centers, dimension, use_bias=use_bias)
+        self.cv5d = PtConv(2 * pl, 2 * pl, n_centers, dimension, use_bias=use_bias)
         # Inputs get concatenated with previous inputs
-        self.cv4d = PtConv(4 * pl, 2 * pl, n_centers, dimension, use_bias=False)
+        self.cv4d = PtConv(4 * pl, 2 * pl, n_centers, dimension, use_bias=use_bias)
 
         # 64 Convolutional kernels + Concatenated inputs
-        self.cv3d = PtConv(4 * pl, pl, n_centers, dimension, use_bias=False)
-        self.cv2d = PtConv(2 * pl, pl, n_centers, dimension, use_bias=False)
-        self.cv1d = PtConv(2 * pl, pl, n_centers, dimension, use_bias=False)
-        self.cv0d = PtConv(2 * pl, pl, n_centers, dimension, use_bias=False)
+        self.cv3d = PtConv(4 * pl, pl, n_centers, dimension, use_bias=use_bias)
+        self.cv2d = PtConv(2 * pl, pl, n_centers, dimension, use_bias=use_bias)
+        self.cv1d = PtConv(2 * pl, pl, n_centers, dimension, use_bias=use_bias)
+        self.cv0d = PtConv(2 * pl, pl, n_centers, dimension, use_bias=use_bias)
 
         # Fully connected layer
         self.fcout = nn.Linear(pl + pl, output_channels)
 
-        self.bn0 = nn.BatchNorm1d(pl, track_running_stats=trs)
-        self.bn1 = nn.BatchNorm1d(pl, track_running_stats=trs)
-        self.bn2 = nn.BatchNorm1d(pl, track_running_stats=trs)
-        self.bn3 = nn.BatchNorm1d(pl, track_running_stats=trs)
-        self.bn4 = nn.BatchNorm1d(2 * pl, track_running_stats=trs)
-        self.bn5 = nn.BatchNorm1d(2 * pl, track_running_stats=trs)
-        self.bn6 = nn.BatchNorm1d(2 * pl, track_running_stats=trs)
+        if norm_type == 'bn':
+            self.bn0 = nn.BatchNorm1d(pl, track_running_stats=trs)
+            self.bn1 = nn.BatchNorm1d(pl, track_running_stats=trs)
+            self.bn2 = nn.BatchNorm1d(pl, track_running_stats=trs)
+            self.bn3 = nn.BatchNorm1d(pl, track_running_stats=trs)
+            self.bn4 = nn.BatchNorm1d(2 * pl, track_running_stats=trs)
+            self.bn5 = nn.BatchNorm1d(2 * pl, track_running_stats=trs)
+            self.bn6 = nn.BatchNorm1d(2 * pl, track_running_stats=trs)
 
-        self.bn5d = nn.BatchNorm1d(2 * pl, track_running_stats=trs)
-        self.bn4d = nn.BatchNorm1d(2 * pl, track_running_stats=trs)
-        self.bn3d = nn.BatchNorm1d(pl, track_running_stats=trs)
-        self.bn2d = nn.BatchNorm1d(pl, track_running_stats=trs)
-        self.bn1d = nn.BatchNorm1d(pl, track_running_stats=trs)
-        self.bn0d = nn.BatchNorm1d(pl, track_running_stats=trs)
+            self.bn5d = nn.BatchNorm1d(2 * pl, track_running_stats=trs)
+            self.bn4d = nn.BatchNorm1d(2 * pl, track_running_stats=trs)
+            self.bn3d = nn.BatchNorm1d(pl, track_running_stats=trs)
+            self.bn2d = nn.BatchNorm1d(pl, track_running_stats=trs)
+            self.bn1d = nn.BatchNorm1d(pl, track_running_stats=trs)
+            self.bn0d = nn.BatchNorm1d(pl, track_running_stats=trs)
+        elif norm_type == 'gn':
+            print("Uses group norm")
+            self.bn0 = nn.GroupNorm(pl // 2, pl)
+            self.bn1 = nn.GroupNorm(pl // 2, pl)
+            self.bn2 = nn.GroupNorm(pl // 2, pl)
+            self.bn3 = nn.GroupNorm(pl // 2, pl)
+            self.bn4 = nn.GroupNorm(pl, 2 * pl)
+            self.bn5 = nn.GroupNorm(pl, 2 * pl)
+            self.bn6 = nn.GroupNorm(pl, 2 * pl)
+
+            self.bn5d = nn.GroupNorm(pl, 2 * pl)
+            self.bn4d = nn.GroupNorm(pl, 2 * pl)
+            self.bn3d = nn.GroupNorm(pl // 2, pl)
+            self.bn2d = nn.GroupNorm(pl // 2, pl)
+            self.bn1d = nn.GroupNorm(pl // 2, pl)
+            self.bn0d = nn.GroupNorm(pl // 2, pl)
 
         self.drop = nn.Dropout(dropout)
         self.relu = nn.ReLU(inplace=True)
@@ -397,6 +415,109 @@ class SegBig(nn.Module):
 
         x0d, _ = self.cv0d(x1d, pts1, 8, input_pts)
         x0d = self.relu(apply_bn(x0d, self.bn0d))
+        x0d = torch.cat([x0d, x0], dim=2)
+
+        xout = x0d
+        xout = self.drop(xout)
+        xout = self.fcout(xout)
+
+        if return_features:
+            return xout, x0d
+        else:
+            return xout
+
+
+##############################################
+# BIG SEGMENTATION NETWORK WITHOUT BATCH NORM
+##############################################
+
+
+class SegBigNoBatch(nn.Module):
+    def __init__(self, input_channels, output_channels, trs=False, dimension=3, dropout=0, use_bias=False):
+        super(SegBigNoBatch, self).__init__()
+
+        n_centers = 16
+
+        # Number of convolutional kernels to start with
+        pl = 64
+
+        # 64 convolutional kernels
+        self.cv0 = PtConv(input_channels, pl, n_centers, dimension, use_bias=use_bias)
+        self.cv1 = PtConv(pl, pl, n_centers, dimension, use_bias=use_bias)
+        self.cv2 = PtConv(pl, pl, n_centers, dimension, use_bias=use_bias)
+        self.cv3 = PtConv(pl, pl, n_centers, dimension, use_bias=use_bias)
+
+        # 128 convolutional kernels
+        self.cv4 = PtConv(pl, 2 * pl, n_centers, dimension, use_bias=use_bias)
+        self.cv5 = PtConv(2 * pl, 2 * pl, n_centers, dimension, use_bias=use_bias)
+        self.cv6 = PtConv(2 * pl, 2 * pl, n_centers, dimension, use_bias=use_bias)
+        self.cv5d = PtConv(2 * pl, 2 * pl, n_centers, dimension, use_bias=use_bias)
+        # Inputs get concatenated with previous inputs
+        self.cv4d = PtConv(4 * pl, 2 * pl, n_centers, dimension, use_bias=use_bias)
+
+        # 64 Convolutional kernels + Concatenated inputs
+        self.cv3d = PtConv(4 * pl, pl, n_centers, dimension, use_bias=use_bias)
+        self.cv2d = PtConv(2 * pl, pl, n_centers, dimension, use_bias=use_bias)
+        self.cv1d = PtConv(2 * pl, pl, n_centers, dimension, use_bias=use_bias)
+        self.cv0d = PtConv(2 * pl, pl, n_centers, dimension, use_bias=use_bias)
+
+        # Fully connected layer
+        self.fcout = nn.Linear(pl + pl, output_channels)
+
+        self.drop = nn.Dropout(dropout)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x, input_pts, return_features=False):
+        """
+        Args:
+            x: Batch of features
+            input_pts: Batch of points
+            return_features: Flag for returning calculated features.
+        """
+        x0, _ = self.cv0(x, input_pts, 16)
+        x0 = self.relu(x0)
+
+        # Number of output points = 2048, Neighborhood of 16 points
+        x1, pts1 = self.cv1(x0, input_pts, 16, 2048)
+        x1 = self.relu(x1)
+
+        x2, pts2 = self.cv2(x1, pts1, 16, 1024)
+        x2 = self.relu(x2)
+
+        x3, pts3 = self.cv3(x2, pts2, 16, 256)
+        x3 = self.relu(x3)
+
+        x4, pts4 = self.cv4(x3, pts3, 8, 64)
+        x4 = self.relu(x4)
+
+        x5, pts5 = self.cv5(x4, pts4, 8, 16)
+        x5 = self.relu(x5)
+
+        x6, pts6 = self.cv6(x5, pts5, 4, 8)
+        x6 = self.relu(x6)
+
+        x5d, _ = self.cv5d(x6, pts6, 4, pts5)
+        x5d = self.relu(x5d)
+        x5d = torch.cat([x5d, x5], dim=2)
+
+        x4d, _ = self.cv4d(x5d, pts5, 4, pts4)
+        x4d = self.relu(x4d)
+        x4d = torch.cat([x4d, x4], dim=2)
+
+        x3d, _ = self.cv3d(x4d, pts4, 4, pts3)
+        x3d = self.relu(x3d)
+        x3d = torch.cat([x3d, x3], dim=2)
+
+        x2d, _ = self.cv2d(x3d, pts3, 8, pts2)
+        x2d = self.relu(x2d)
+        x2d = torch.cat([x2d, x2], dim=2)
+
+        x1d, _ = self.cv1d(x2d, pts2, 8, pts1)
+        x1d = self.relu(x1d)
+        x1d = torch.cat([x1d, x1], dim=2)
+
+        x0d, _ = self.cv0d(x1d, pts1, 8, input_pts)
+        x0d = self.relu(x0d)
         x0d = torch.cat([x0d, x0], dim=2)
 
         xout = x0d
