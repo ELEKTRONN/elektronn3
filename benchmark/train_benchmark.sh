acc04c46c9b4e0f8fe83d0127daff1b046198272
mdraw <martin.drawitsch@gmail.com>
2021-05-24 03:36:22 +0200

    Add training benchmark sbatch script, adjust pred

--- benchmark/train_benchmark.sh
+++ benchmark/train_benchmark.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -l
 
-#SBATCH -o ./outputs/pred-out.%j
-#SBATCH -e ./outputs/pred-err.%j
+#SBATCH -o ./outputs/train-out.%j
+#SBATCH -e ./outputs/train-err.%j
 #SBATCH -D ./
-#SBATCH -J E3_PRED_GPU_TEST
+#SBATCH -J E3_TRAIN_GPU_TEST
 #SBATCH --constraint="gpu"
 #SBATCH --gres=gpu:v100:2
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=00:30:00
+#SBATCH --time=01:30:00
 
 module purge
 module load anaconda/3/2020.02
 
 conda activate e3
 
-srun python3 ./pred_benchmark.py
+srun python3 ./train_benchmark.py
