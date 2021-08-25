40e2796cf748b102bd88ba29cf15c223cf3c855a
filippo <f.kiessler@campus.lmu.de>
2021-08-25 19:10:57 +0200

    added vigra functions but weird problems with the gaussianDivergence of the VDT

--- lsd/testing_lsd.py
+++ lsd/testing_lsd.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 from scipy import ndimage as im
+import vigra as v
 
 one = np.array([[0,0,0,0],
                 [0,0,1,1],
@@ -45,7 +46,25 @@ coords = np.mgrid[:shape[0], :shape[1], :shape[2]]
 coords[:, array==0]=0
 com_lsd = np.copy(coords).astype(float)
 for i in np.unique(labels)[1:]:
-    lsd[:, labels==i] = np.tile(com[i-1].reshape(-1,1), lsd[:, labels==i].shape[1])
+    com_lsd[:, labels==i] = np.tile(com[i-1].reshape(-1,1), com_lsd[:, labels==i].shape[1])
 
 #################################################################
 #################################################################
+
+#################################################################
+######## Vigranumpy VectorDistanceTransform, gaussianDivergence##
+array = array.reshape((1,)+array.shape)
+print("new segmentation like array shape: {}".format(array.shape))
+vigra_array = v.taggedView(array, 'cxyz')
+vdt = v.filters.boundaryVectorDistanceTransform(vigra_array.astype(np.uint32))
+print("vector distance transform shape: {}".format(vdt.shape))
+print("vector distance transform axistags: {}".format(vdt.axistags))
+
+norm_vdt = np.linalg.norm(vdt, axis=0)
+print("vector distance transform (normalised) shape: {}".format(norm_vdt.shape))
+
+gaussian_divergence = v.filters.gaussianDivergence(v.VigraArray(array, axistags=v.defaultAxistags("cxyz")))
+print("gaussian divergence shape: {}".format(gaussian_divergence.shape))
+narray= np.random.rand(3,20,20,10)
+vigra_array=v.VigraArray(narray, axistags=v.defaultAxistags("cxyz"))
+divergence = v.filters.gaussianDivergence(vigra_array)
